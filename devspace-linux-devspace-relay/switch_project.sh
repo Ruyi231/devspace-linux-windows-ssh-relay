c097d18f29aaaa657842385d4d6d222be98c11ba
Ruyi1314 <131221955+Ruyi231@users.noreply.github.com>
2026-07-11 06:59:18 +0800

    Delete devspace-linux-devspace-relay directory

--- devspace-linux-devspace-relay/switch_project.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-"${SCRIPT_DIR}/set_project.sh" "$@"
-"${SCRIPT_DIR}/stop.sh" --quiet || true
-exec "${SCRIPT_DIR}/start.sh"
